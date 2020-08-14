40a415ef957a696439066b7fccbd6cb0a4c7b5e2
Halbird <halbird971@gmail.com>
2020-08-14 22:48:26 +0900

    Initial commit

---
+++ android/src/com/project/cave/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.project.cave;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.project.cave.ProjectCave;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectCave(), config);
+	}
+}
